8fce415b299d320f2fb76a3aa88dae6a75266c8f
kuwingto <kuwingto>
2020-07-24 15:20:11 +0800

    refactoring messageConsole

--- app/static/js/messageConsole.js
+++ app/static/js/messageConsole.js
@@ -1,26 +1,21 @@
-/********
-   message window
-   **********/
-var maxLineNum = 10;
-var currentLineNum = 0;
-var queueText = [];
-var messageConsoleDocumentId;
+function messageConsole(id, maxlinenum) {
+    this.DocumentId = id;
+    this.maxLineNum = maxlinenum;
+    this.currentLineNum = 0;
+    this.queueText = [];
 
-function appendLineToTextArea(s) {
-    if (currentLineNum > maxLineNum) {
-        queueText.shift()
+    this.addLoggingLine = function (msg) {
+        if (this.currentLineNum > this.maxLineNum) {
+            this.queueText.shift()
+        }
+        this.currentLineNum = this.currentLineNum + 1;
+        this.queueText.push(msg);
+        var temptxt = "";
+        for (i = 0; i < this.queueText.length; i++) {
+            temptxt = temptxt + this.queueText[i] + '\n';
+        }
+        var txt = $(this.DocumentId);
+        txt.val(temptxt)
+        $(this.DocumentId).scrollTop($(this.DocumentId)[0].scrollHeight);
     }
-    currentLineNum = currentLineNum + 1;
-    queueText.push(s);
-    var temptxt = "";
-    for (i = 0; i < queueText.length; i++) {
-        temptxt = temptxt + queueText[i] + '\n';
-    }
-    var txt = $(messageConsoleDocumentId);
-    txt.val(temptxt)
-    $(messageConsoleDocumentId).scrollTop($(messageConsoleDocumentId)[0].scrollHeight);
-}
-
-function setActiveMessageConsole(id) {
-    messageConsoleDocumentId = id;
 }
